bf3d9c7cb6b4697d723a6e74b2156449665acf26
Clara Kim <claraj@fb.com>
2020-06-24 12:44:03 -0400

    Initial Commit

---
+++ Flixing/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Flixing
+//
+//  Created by Clara Kim on 6/24/20.
+//  Copyright © 2020 Clara Kim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Flixing/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Flixing
+//
+//  Created by Clara Kim on 6/24/20.
+//  Copyright © 2020 Clara Kim. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
