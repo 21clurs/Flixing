426f5d6155b3eacc5354df124e0e2ec9307b5066
Clara Kim <claraj@fb.com>
2020-06-25 15:15:41 -0400

    Loading state and network error alert message added

--- Flixing/ViewControllers/MoviesViewController.m
+++ Flixing/ViewControllers/MoviesViewController.m
@@ -16,6 +16,7 @@
 @property (nonatomic, strong) NSArray *movies;
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
 @property (nonatomic, strong) UIRefreshControl *refreshControl;
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *activityIndicator;
 
 @end
 
@@ -39,6 +40,8 @@
 }
 
 - (void)fetchMovies {
+    
+    [self.activityIndicator startAnimating];
     // Making the network call
     // Setup
     NSURL *url = [NSURL URLWithString:@"https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed"];
@@ -48,6 +51,14 @@
     NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
            if (error != nil) {
                NSLog(@"%@", [error localizedDescription]);
+               UIAlertController *alert = [UIAlertController alertControllerWithTitle: @"Cannot Get Movies" message:@"The internet connection appears to be offline" preferredStyle: (UIAlertControllerStyleAlert)];
+               UIAlertAction *tryAgainAction = [UIAlertAction actionWithTitle:@"Try Again" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+                   // Handling the Try Again response here
+                   [self fetchMovies];
+               }];
+               // Add the OK action to the alert controller
+               [alert addAction:tryAgainAction];
+               [self presentViewController:alert animated:YES completion:^{}]; // What to do with block syntax when you don't want to do anything?
            }
            else {
                NSDictionary *dataDictionary = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
@@ -61,6 +72,7 @@
                
                // Calls datasource method again in case the underlying data has changed
                [self.tableView reloadData];
+               [self.activityIndicator stopAnimating];
                // TODO: Get the array of movies
                // TODO: Store the movies in a property to use elsewhere
                // TODO: Reload your table view data
